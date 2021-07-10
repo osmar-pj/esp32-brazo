e5b5b9bf86de232a2c3e6d2c6299457b3ad3ff7b
osmar-pj <osmar.pj@hotmail.com>
2021-07-10 08:54:14 -0500

    conexion entre esp32 y server nodeJS

--- esp32-brazo-robot.ino
+++ esp32-brazo-robot.ino
@@ -1,22 +1,16 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
-#include <Servo.h>
+#include <ESP32Servo.h>
 
 // Objeto Servo
 Servo myservo;
 int pos = 0;
+int servoPin = 13;
 
 // Update these with values suitable for your network.
 // variables para control de led y alarmas
-int IN_PRESENCE_1 = 4;    // D2
-int IN_DOOR_1 = 5;        // D1
-//int IN_PRESENCE_2 = 14;
-//int IN_DOOR_2 = 12;
-int thisState_1 = 0;
-int lastState_1 = 0;
-int thisState_2 = 0;
-int lastState_2 = 0;
+
 String stringData;
 char charData[1024];
 
@@ -30,7 +24,7 @@ const char* password = "liz.jove73031792";
 
 // MQTT
 const char *mqtt_broker = "143.198.128.180";
-const char *topic_alarm = "peru/arequipa/hunter/palomino_florez/piso_1";
+const char *topic_alarm = "robot/brazo";
 const char *mqtt_username = "admin";
 const char *mqtt_password = "public";
 const int mqtt_port = 1883;
@@ -39,10 +33,7 @@ WiFiClient espClient;
 PubSubClient client(espClient);
 
 void setup() {
-
-  pinMode(IN_DOOR_1, INPUT);
-  pinMode(IN_PRESENCE_1, INPUT);
-  myservo.attach(13);
+  myservo.attach(servoPin);
   Serial.begin(115200);
 
   WiFi.begin(ssid, password);
@@ -58,6 +49,7 @@ void setup() {
 void reconnected() {
   // connecting to mqqt broker
   client.setServer(mqtt_broker, mqtt_port);
+  client.setCallback(callback);
   while(!client.connected()) {
     String client_id = "ESP-";
     client_id += String(WiFi.macAddress());
@@ -74,7 +66,7 @@ void reconnected() {
     }
   }
   // publish and suscribe
-  //client.subscribe(topic_alarm_presence);
+  client.subscribe(topic_alarm);
   Serial.println("cuando termine de reactivarse");
 }
 
@@ -84,28 +76,23 @@ void loop() {
     reconnected();
   }
   client.loop();
-  myservo.write(50);
   delay(15);
   esp_mac = WiFi.macAddress();
-  thisState_1 = digitalRead(IN_PRESENCE_1);
-  delay(100);
-  thisState_2 = digitalRead(IN_DOOR_1);
-  delay(300);
-  char alarm[300];
-  StaticJsonDocument<100> doc;
-  if (thisState_1 != lastState_1 || thisState_2 != lastState_2) {
-    lastState_1 = thisState_1;
-    lastState_2 = thisState_2;
-    doc["alarm"]["esp"] = esp_mac;
-    doc["alarm"]["presence"] = String(thisState_1);
-    doc["alarm"]["door"] = String(thisState_2);
-    size_t m = serializeJson(doc, alarm);
-    serializeJson(doc, Serial);
-    client.publish(topic_alarm, alarm);
-    delay(1000);
-  }
   if (WiFi.status() != WL_CONNECTED) {
     //Serial.println("no conectado");
     ESP.restart();
   }
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived in topic: ");
+  String message;
+  for (int i = 0; i < length; i++) {
+    message = message + (char) payload[i];
+  }
+  Serial.print(message);
+  myservo.write(message.toInt());
+//   if (message == "true") { digitalWrite(LED, HIGH); }   // Value servomotor
+  Serial.println();
+  Serial.println("-------------------------");
 }
